02560defad0ae8875423c7004ed08aa4ca25f920
rainnfx <matteo.aure@gmail.com>
2023-07-23 10:39:03 -0700

    nothing new rlly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
       </section>
       <footer>
         <div className="w-footer mt-32 mx-auto">
-          <p>temp text</p>
           <p className="text-comment mb-7">Copyright © 2023 <a className="text-offer-white left-10 font-semibold transition-all duration-500 ease-in-out hover:text-off-white hover:cursor-pointer">@rainnfx</a></p>
         </div>
       </footer>
