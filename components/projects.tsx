351ead2aaf6da61c62037ec7b7f2f68ba393ee7c
rainnfx <matteo.aure@gmail.com>
2023-08-15 09:54:35 -0700

    made tiny changes to ease my brain

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,10 +1,10 @@
 const Projects = () => {
   return (
-    <section className="hero w-section h-screen flex flex-col mt-32 mx-auto">
+    <section className="hero w-section h-screen flex flex-col mx-auto">
       <div className="flex items-center">
-        <h2 className="text-2xl text-light-slate font-mono mt-1 mr-2">02.</h2>
-        <h1 className="text-3xl font-bold text-off-white mr-2">Projects</h1>
-        <hr className="text-comment rounded-md"></hr>
+        <h2 className="text-2xl text-light-slate font-mono mr-2">02.</h2>
+        <h1 className="text-3xl font-bold text-off-white mr-2 mb-2">Projects</h1>
+        <hr className="text-comment w-64 rounded-md"/>
       </div>
 
       </section>
